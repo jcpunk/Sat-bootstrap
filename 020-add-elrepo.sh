31579c63a5c93cc8dcb97d63995850f933aebf35
Pat Riehecky <riehecky@fnal.gov>
2016-02-19 13:16:48 -0600

    more fixes to streamline things

--- 020-add-elrepo.sh
+++ 020-add-elrepo.sh
@@ -7,22 +7,22 @@ hammer gpg create --name="ELRepo" --key=/tmp/elrepo.pubkey  --organization=${MY_
 
 hammer product create --gpg-key="ELRepo" --name='ELRepo' --organization=${MY_ORG}
 
-hammer content-view create --name=cv-software-elrepo-kmods-el6 --organization=Fermilab
-hammer content-view create --name=cv-software-elrepo-kmods-el7 --organization=Fermilab
-hammer content-view create --name=cv-software-elrepo-kernels-el6 --organization=Fermilab
-hammer content-view create --name=cv-software-elrepo-kernels-el7 --organization=Fermilab
-hammer content-view publish --async --organization=Fermilab --description=Empty --name=cv-software-elrepo-kmods-el6
-hammer content-view publish --async --organization=Fermilab --description=Empty --name=cv-software-elrepo-kmods-el7
-hammer content-view publish --async --organization=Fermilab --description=Empty --name=cv-software-elrepo-kernels-el6
-hammer content-view publish --async --organization=Fermilab --description=Empty --name=cv-software-elrepo-kernels-el7
-hammer repository create --gpg-key="ELRepo" --name='ELRepo kmods for EL7 - x86_64' --organization=Fermilab --product='ELRepo' --content-type='yum' --publish-via-http=true --url=http://ftp.osuosl.org/pub/elrepo/elrepo/el7/x86_64/
-hammer repository create --gpg-key="ELRepo" --name='ELRepo kernels for EL7 - x86_64' --organization=Fermilab --product='ELRepo' --content-type='yum' --publish-via-http=true --url=http://ftp.osuosl.org/pub/elrepo/kernel/el7/x86_64/
-hammer repository create --gpg-key="ELRepo" --name='ELRepo kmods for EL6 - x86_64' --organization=Fermilab --product='ELRepo' --content-type='yum' --publish-via-http=true --url=http://ftp.osuosl.org/pub/elrepo/elrepo/el6/x86_64/
-hammer repository create --gpg-key="ELRepo" --name='ELRepo kernels for EL6 - x86_64' --organization=Fermilab --product='ELRepo' --content-type='yum' --publish-via-http=true --url=http://ftp.osuosl.org/pub/elrepo/kernel/el6/x86_64/
-hammer content-view add-repository --organization=Fermilab --product=ELRepo --repository='ELRepo kmods for EL7 - x86_64' --name=cv-software-elrepo-kmods-el7
-hammer content-view add-repository --organization=Fermilab --product=ELRepo --repository='ELRepo kmods for EL6 - x86_64' --name=cv-software-elrepo-kmods-el6
-hammer content-view add-repository --organization=Fermilab --product=ELRepo --repository='ELRepo kernels for EL6 - x86_64' --name=cv-software-elrepo-kernels-el6
-hammer content-view add-repository --organization=Fermilab --product=ELRepo --repository='ELRepo kernels for EL7 - x86_64' --name=cv-software-elrepo-kernels-el7
+hammer content-view create --name=cv-software-elrepo-kmods-el6 --organization=${MY_ORG}
+hammer content-view create --name=cv-software-elrepo-kmods-el7 --organization=${MY_ORG}
+hammer content-view create --name=cv-software-elrepo-kernels-el6 --organization=${MY_ORG}
+hammer content-view create --name=cv-software-elrepo-kernels-el7 --organization=${MY_ORG}
+hammer content-view publish --async --organization=${MY_ORG} --description=Empty --name=cv-software-elrepo-kmods-el6
+hammer content-view publish --async --organization=${MY_ORG} --description=Empty --name=cv-software-elrepo-kmods-el7
+hammer content-view publish --async --organization=${MY_ORG} --description=Empty --name=cv-software-elrepo-kernels-el6
+hammer content-view publish --async --organization=${MY_ORG} --description=Empty --name=cv-software-elrepo-kernels-el7
+hammer repository create --gpg-key="ELRepo" --name='ELRepo kmods for EL7 - x86_64' --organization=${MY_ORG} --product='ELRepo' --content-type='yum' --publish-via-http=true --url=http://ftp.osuosl.org/pub/elrepo/elrepo/el7/x86_64/
+hammer repository create --gpg-key="ELRepo" --name='ELRepo kernels for EL7 - x86_64' --organization=${MY_ORG} --product='ELRepo' --content-type='yum' --publish-via-http=true --url=http://ftp.osuosl.org/pub/elrepo/kernel/el7/x86_64/
+hammer repository create --gpg-key="ELRepo" --name='ELRepo kmods for EL6 - x86_64' --organization=${MY_ORG} --product='ELRepo' --content-type='yum' --publish-via-http=true --url=http://ftp.osuosl.org/pub/elrepo/elrepo/el6/x86_64/
+hammer repository create --gpg-key="ELRepo" --name='ELRepo kernels for EL6 - x86_64' --organization=${MY_ORG} --product='ELRepo' --content-type='yum' --publish-via-http=true --url=http://ftp.osuosl.org/pub/elrepo/kernel/el6/x86_64/
+hammer content-view add-repository --organization=${MY_ORG} --product=ELRepo --repository='ELRepo kmods for EL7 - x86_64' --name=cv-software-elrepo-kmods-el7
+hammer content-view add-repository --organization=${MY_ORG} --product=ELRepo --repository='ELRepo kmods for EL6 - x86_64' --name=cv-software-elrepo-kmods-el6
+hammer content-view add-repository --organization=${MY_ORG} --product=ELRepo --repository='ELRepo kernels for EL6 - x86_64' --name=cv-software-elrepo-kernels-el6
+hammer content-view add-repository --organization=${MY_ORG} --product=ELRepo --repository='ELRepo kernels for EL7 - x86_64' --name=cv-software-elrepo-kernels-el7
 
-hammer product set-sync-plan --sync-plan='6pm Fri' --organization=Fermilab --name='ELRepo'
+hammer product set-sync-plan --sync-plan='6pm Fri' --organization=${MY_ORG} --name='ELRepo'
 
